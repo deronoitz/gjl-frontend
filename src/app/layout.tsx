dff999bda262fafbdbb5e4def56ffc4ede25cffb
Widi Baskoro Aji <widi@flip.id>
2025-09-08 21:03:03 +0700

    fix: update theme color in manifest and add theme-color meta tag in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="GJL App" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
       </head>
       <body
